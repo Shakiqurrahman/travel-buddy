1c5e1e3cd0ccaa276979ce1bac237bd88d8b413c
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-06-14 01:39:39 +0600

    react icons added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { FaRegUser } from "react-icons/fa6";
+
 
 const Navbar = () => {
     return (
-        <header className="h-20 flex justify-between items-center max-container">
+        <header className="h-24 flex justify-between items-center max-container">
             <h1 className="text-2xl font-semibold text-black">
                 Travel <span className="text-[#30af5b]">Buddy.</span>
             </h1>
@@ -14,7 +16,8 @@ const Navbar = () => {
                     <li>Contact Us</li>
                 </ul>
             </nav>
-            <button className="btn_black">
+            <button className="btn_black flex items-center gap-3">
+                <FaRegUser className="font-thin text-[22px]"/>
                 Login
             </button>
         </header>
